87aeeb0a9092e423ca08201a2a2797c6d93b9410
Pedro Daniel <110742057+pedaniiel@users.noreply.github.com>
2023-08-15 11:59:44 -0300

    q3.c

--- q3.c
+++ q3.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-int ladosPitagoricos(int limit){
-    int c, a, b;
-    int i;
-    int j;
-    for (i = 1; i < limit; i++){
-        for(j = 1; j < limit;j++){
-            
-            a = pow(i,2);
-            b = pow(j,2);
-            c = sqrt((a+b));
-            if((a+b) == pow(c,2)){
-                printf("%i %i %i\n", i, j, c);
+
+// Função para encontrar triplas pitagóricas com limite 'limit'
+int ladosPitagoricos(int limit) {
+    int c, a, b;     // Variáveis para armazenar os lados do triângulo pitagórico
+    int i, j;        // Variáveis de loop para percorrer os valores possíveis
+
+    for (i = 1; i < limit; i++) {  // Loop externo para o valor de 'i'
+        for (j = 1; j < limit; j++) {  // Loop interno para o valor de 'j'
+
+            a = pow(i, 2);  // Calcula o quadrado de 'i' (lado a)
+            b = pow(j, 2);  // Calcula o quadrado de 'j' (lado b)
+            c = sqrt((a + b));  // Calcula a hipotenusa 'c' usando a fórmula de Pitágoras
+
+            if ((a + b) == pow(c, 2)) {  // Verifica se a soma dos quadrados dos lados é igual ao quadrado da hipotenusa
+                printf("%i %i %i\n", i, j, c);  // Imprime a tripla pitagórica
             }
         }
     }
 }
 
-int main(){
-ladosPitagoricos(20);
+int main() {
+    ladosPitagoricos(20);  // Chama a função para encontrar triplas pitagóricas com limite 20
 }
